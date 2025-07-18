81be36a5fc163a05194c9e5b73d525cfb6a6d57f
DevPsico <ericsonsergio19844@gmail.com>
2025-07-17 23:08:32 -0300

    Criado a Classe PackageDto

---
+++ DTOs/PackageDto.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------------
+// Autor: Ericson Sérgio Costa Soares
+// Data de Criação: 17/07/2025
+// Arquivo: PacoteDto
+// Projeto: TravelAgency
+// Descrição: Pega a Entidade Package e converte em PackageDto
+// -----------------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs
+{
+    public class PackageDto
+
+    {
+        public string Titulo { get; set; }
+
+        public string Descricao { get; set; }
+       
+        public string Destino { get; set; }
+
+        public int DuracaoDias { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataFim { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public string? ImagemUrl { get; set; }
+    }
+}
