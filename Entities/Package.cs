592734db7457a6b9782e3f212fb192d17f5cd741
DevPsico <ericsonsergio19844@gmail.com>
2025-07-17 22:15:06 -0300

    Criado a classe Package

---
+++ Entities/Package.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Entities
+{
+    public class Package
+    {
+        public int ID_Pacote { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Destino { get; set; }
+        public int Duracao { get; set; }
+        public string Datas_Disponiveis { get; set; }
+        public decimal Valor { get; set; }
+
+        /*
+    public ICollection<Reserva> Reservas { get; set; }
+    public ICollection<Avaliacao> Avaliacoes { get; set; }
+        */
+}
+
+}
