b36e5080c193f7819a76b40e0c00256b061cda6f
DevPsico <ericsonsergio19844@gmail.com>
2025-07-19 08:11:35 -0300

    Criado a Classe ReservationDto

---
+++ DTOs/ReservationDto.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------------
+// 🧠 Autor: Ericson Sérgio Costa Soares
+// 📅 Data de Criação: 19/07/2025
+// 📁 Arquivo: ReservationDto
+// 📦 Projeto: TravelAgency
+// 🚀  Descrição: Pega a Entidade Reservation e converte em ReservationDto
+// -----------------------------------------------------------------------------
+
+namespace WebApplication1.DTOs
+{
+    public class ReservationDto
+
+    {
+        public int ID_Reserva { get; set; }
+        public DateTime Data_Reserva { get; set; }
+        public StatusReserva Status { get; set; }
+        public string Numero_Reserva { get; set; }
+        public int ID_Usuario { get; set; }
+        public int ID_Pacote { get; set; }
+    }
+}
